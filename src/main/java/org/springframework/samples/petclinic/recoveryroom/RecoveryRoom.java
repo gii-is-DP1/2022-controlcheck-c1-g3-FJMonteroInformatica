29689d4329769518fdd6014809a937ed2e19736d
Francisco Montero <fj.montero.informatica@gmail.com>
2022-11-03 18:19:37 +0100

    Examen terminado

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoom.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoom.java
@@ -1,17 +1,36 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
-import javax.persistence.Transient;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class RecoveryRoom {
-    Integer id;
+@Entity
+@Table(name="RecoveryRooms")
+public class RecoveryRoom extends BaseEntity {
+	
+	@Column(nullable=false)
+	@Size(min=3, max=50)
     String name;
+	
+	@Column(nullable=false)
+    @Min(0)
     double size;
+	
+	@Column(nullable=false)
     boolean secure;
-    @Transient
+    
+    @ManyToOne
+    @JoinColumn(name = "roomType")
     RecoveryRoomType roomType;
 }
